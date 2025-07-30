df2be47feeb1b71077452466938473c481cdc55e
Hashemeee92 <hashemeee92@gmail.com>
2025-07-30 18:15:04 +0600

    Grade is calculated

---
+++ grade.js
@@ -0,0 +1,22 @@
+// Letter grade and marks
+//A: 90-100
+//B:80-89
+// C: 70-79
+//D: 60-69
+//F: 0-59
+let marks = 96;
+if(marks>=90 && marks<=100){
+console.log('Grade is A:', marks);
+}
+else if(marks>=80 && marks<=89){
+    console.log('Grade is B:', marks);
+}
+else  if(marks>=70 && marks<=79){
+    console.log('Grade is C :', marks)
+}
+else if(marks>=60 && marks<=69){
+    console.log('Your Grade is: D and marks is:', marks)
+}
+else{
+    console.log('Your are failed');
+}
